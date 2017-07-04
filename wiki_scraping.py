a975164655b9b2c6725aaada7602ff0d928ce9ec
casol <frombitstobytes.com@gmail.com>
2017-07-04 23:42:47 +0200

    Update URL extracting

--- wiki_scraping.py
+++ wiki_scraping.py
@@ -8,16 +8,33 @@ html = urlopen("https://en.wikipedia.org/wiki/List_of_museum_ships")
 bs_obj = BeautifulSoup(html, "html.parser")
 
 table = bs_obj.findAll("table", {"class": "wikitable"})[0]
+
+"""
+urls = []
+for tr in table.findAll('tr'):
+    td = tr.find('td')
+    urls.append(td)
+"""
+links = [tr.find('td') for tr in table.findAll('tr')]
+
+
+"""
 rows = []
 # easy solution best solution ?
 for row in table.findAll("tr"):
-    rows.append(row)
+    for cell in row.findAll("td"):
+        for link in cell.findAll("a", href=re.compile("^(/wiki/)")):
+            print(link)
+            #if 'href' in link.attrs[0]:
+                #print(link.attrs['href'])
 
-print(rows[529])
+        #print(cell)
+
+#print(rows[1])
     #for link in t.findAll("a", href=re.compile("^(/wiki/)")):
         #if 'href' in link.attrs:
             #print(link.attrs['href'])
-
+"""
 
 """
 
