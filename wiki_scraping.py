916455ff5cab9e199e4a9a717b54e7d705f60ec7
casol <frombitstobytes.com@gmail.com>
2017-07-06 22:59:15 +0200

    Update URLs format

--- wiki_scraping.py
+++ wiki_scraping.py
@@ -1,28 +1,38 @@
+# Scraping list of museum ships with URLs
 import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import csv
 
-
+# open url
 html = urlopen("https://en.wikipedia.org/wiki/List_of_museum_ships")
+# create BeautifulSoup object
 bs_obj = BeautifulSoup(html, "html.parser")
-
+# find wikitable
 table = bs_obj.findAll("table", {"class": "wikitable"})[0]
 
 # urls
 urls = []
 for tr in table.findAll('tr'):
+    # select all first <td> inside all <tr>
     td = tr.find('td')
     try:
+        # find all URLs starting with /wiki/ or /w/
         for link in td.findAll("a", href=re.compile("^(/wiki/)|^(/w/)")):
-            if 'href' in link.attrs:
-                print(link.attrs['href'])
-                urls.append(link)
+            if 'href' in link.attrs:  # returns a Python dictionary object
+                # add to the list
+                urls.append('https://en.wikipedia.org' + link.attrs['href'])
+                #print(link.attrs['href'], urls.index(link))
     except AttributeError as e:
         # without title row
         pass
 
+urls.insert(23, 'ANGARA')
+for url in urls:
+    print(url)
 
+
+"""
 #links = [tr.find('td') for tr in table.findAll('tr')]
 
 # table
@@ -38,3 +48,4 @@ try:
             writer.writerow(csv_row)
 finally:
     csv_file.close()
+"""
